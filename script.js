07b23fd2afb395efaa40b082ba3e9b207ba15929
DhayananthG <iamdhayaoffl@gmail.com>
2025-01-29 20:26:40 +0530

    Projects Section Completed

--- script.js
+++ script.js
@@ -38,8 +38,42 @@ window.addEventListener('scroll', () => {
 
 window.addEventListener("load", function() {
     let loader = document.querySelector(".code-loader");
-    loader.classList.add("hidden");
+
+    loader.classList.add("fade-out");
+
     setTimeout(() => {
-        loader.style.display = "none"; 
-    }, 800); // 500ms for the fade effect
+        loader.style.display = "none";
+    }, 500); 
+});
+
+
+const scriptURL = "https://script.google.com/macros/s/AKfycbw8lrd_i81uKeBMbVoxAbMjY8J1U1atCgIwOcO4TeJICcLA8ylzb5gCj_sXseAK2Tkrqg/exec";
+const form = document.forms["submit-to-google-sheet"];
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    var formData = new FormData(form);
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var message = document.getElementById("message").value;
+
+    // Append values to the formData
+    formData.append("Name", name);
+    formData.append("Email", email);
+    formData.append("Message", message);
+    console.log('FormData:', formData);
+
+    fetch(scriptURL, { method: "POST", body: formData })
+        .then((response) => {
+            formData.forEach((value, key) => {
+                console.log(key + ": " + value);
+            });
+            msgg.innerHTML = "Message Sent Successfully";
+            form.reset();
+            setTimeout(function () {
+                msgg.innerHTML = "";
+            }, 4000);
+        })
+        .catch((error) => {
+            msgg.innerHTML = "Error Occurred";
+        });
 });
